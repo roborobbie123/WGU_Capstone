bbbd56e4900973d5d8f95d01ae5660df6c063d6a
Robert Barrett <roborobbie123@gmail.com>
2025-08-07 10:09:46 -0400

    built the SVR model and the performance metrics

--- fantasy.py
+++ fantasy.py
@@ -7,7 +7,7 @@ dataset = pd.read_csv('archive-2/yearly_player_stats_offense.csv')
 # -------- DATA FILTERS -------------------------------------------------------
 
 # Filter by position
-position = 'te'
+position = 'wr'
 dataset['position'] = dataset['position'].astype(str).str.strip().str.lower()
 dataset = dataset[dataset['position'] == position]
 
@@ -16,8 +16,8 @@ dataset = dataset[dataset['position'] == position]
 # dataset = dataset[dataset['age'] >= age]
 
 # Filtering by seasons played
-# seasons_played = 0
-# dataset = dataset[dataset['seasons_played'] >= seasons_played]
+seasons_played = 5
+dataset = dataset[dataset['seasons_played'] >= seasons_played]
 
 # Filtering by targets
 # targets = 20
@@ -84,7 +84,6 @@ data['next_year_points'] = data.groupby('player_name')['fantasy_points_ppr'].shi
 data = data.dropna()
 data = data[(data['games_played_career'] > 0) & (data['games_played_season'] > 0)]
 
-
 # Features and dependent variable
 X = data[[
     # Demographics
@@ -117,19 +116,14 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
 
+# BUILDING THE MODELS
+
 # Building Linear Regression Model
 from sklearn.linear_model import LinearRegression
 
 lr = LinearRegression()
 lr.fit(X_train, y_train)
 y_pred = lr.predict(X_test)
-
-# Performance Metrics
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
-print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
-print("Mean Absolute Error:", mean_absolute_error(y_test, y_pred))
-print("R² Score:", r2_score(y_test, y_pred))
 
 # Building Random Forest Model
 from sklearn.ensemble import RandomForestRegressor
@@ -138,6 +132,47 @@ rf = RandomForestRegressor(n_estimators=100, random_state=0)
 rf.fit(X_train, y_train)
 rf_pred = rf.predict(X_test)
 
-print("Mean Squared Error:", mean_squared_error(y_test, rf_pred))
-print("Mean Absolute Error:", mean_absolute_error(y_test, rf_pred))
-print("R² Score:", r2_score(y_test, rf_pred))
+# Building SVR Model
+
+from sklearn.preprocessing import StandardScaler
+scaler_X = StandardScaler()
+scaler_y = StandardScaler()
+X_trained_scaled = scaler_X.fit_transform(X_train)
+X_test_scaled = scaler_X.fit_transform(X_test)
+y_trained_scaled = scaler_y.fit_transform(y_train.reshape(-1, 1)).ravel()
+
+from sklearn.svm import SVR
+svr = SVR(kernel = 'rbf')
+svr.fit(X_trained_scaled, y_trained_scaled)
+svr_pred_scaled = svr.predict(X_test_scaled)
+svr_pred = scaler_y.inverse_transform(svr_pred_scaled.reshape(-1, 1))
+
+
+# PERFORMANCE METRICS
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+lr_mse = mean_squared_error(y_test, y_pred)
+lr_mae = mean_absolute_error(y_test, y_pred)
+lr_r2 = r2_score(y_test, y_pred)
+print("\n---Linear Regression---")
+print("Mean Squared Error:", lr_mse)
+print("Mean Absolute Error:", lr_mae)
+print("R² Score:", lr_r2)
+
+rf_mse = mean_squared_error(y_test, rf_pred)
+rf_mae = mean_absolute_error(y_test, rf_pred)
+rf_r2 = r2_score(y_test, rf_pred)
+print("\n---Random Forest---")
+print("Mean Squared Error:", rf_mse)
+print("Mean Absolute Error:", rf_mae)
+print("R² Score:", rf_r2)
+
+svr_mse = mean_squared_error(y_test, svr_pred)
+svr_mae = mean_absolute_error(y_test, svr_pred)
+svr_r2 = r2_score(y_test, svr_pred)
+print("\n---SVR---")
+print("Mean Squared Error:", svr_mse)
+print("Mean Absolute Error:", svr_mae)
+print("R² Score:", svr_r2)
+
+
